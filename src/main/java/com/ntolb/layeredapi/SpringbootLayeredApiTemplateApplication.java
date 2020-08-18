3fa678fccd94e1601e152fc02ef2d5112a870874
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-18 12:57:13 -0400

    08182020 -- root/* -- FIRST COMMIT

---
+++ src/main/java/com/ntolb/layeredapi/SpringbootLayeredApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ntolb.layeredapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLayeredApiTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLayeredApiTemplateApplication.class, args);
+	}
+
+}

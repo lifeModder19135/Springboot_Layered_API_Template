f4d860d8547e5aae0aa488b6ce2085c9c2fe8dba
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-18 14:20:30 -0400

    08182020 -- layeredapi/...application.java -- Added class-scoped annotations; added autowired repository property declaration; Started adding mapping methods for /register; -- layeredapi/data/UserRepository.java -- Created class; implementation of JpaRepository; -- layeredapi/model/user.java -- Created class; simple POJO;

--- src/main/java/com/ntolb/layeredapi/SpringbootLayeredApiTemplateApplication.java
+++ src/main/java/com/ntolb/layeredapi/SpringbootLayeredApiTemplateApplication.java
@@ -1,10 +1,26 @@
 package com.ntolb.layeredapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ntolb.layeredapi.data.UserRepository;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin(origins = "*")
 public class SpringbootLayeredApiTemplateApplication {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@PostMapping("/register")
+	public String register(@RequestBody User user) {
+		repository.save
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLayeredApiTemplateApplication.class, args);

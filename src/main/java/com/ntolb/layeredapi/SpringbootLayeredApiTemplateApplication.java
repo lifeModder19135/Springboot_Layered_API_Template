3631521f83e2007b03d3407ab00bc99007c7d458
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-18 15:19:27 -0400

    08182020 -- layeredapi/...Application.java -- added more api controller mapping methods; -- layeredapi/data/UserRepository.java -- Added placeholder interface method implementations;

--- src/main/java/com/ntolb/layeredapi/SpringbootLayeredApiTemplateApplication.java
+++ src/main/java/com/ntolb/layeredapi/SpringbootLayeredApiTemplateApplication.java
@@ -1,13 +1,20 @@
 package com.ntolb.layeredapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ntolb.layeredapi.data.UserRepository;
+import com.ntolb.layeredapi.model.User;
 
 @SpringBootApplication
 @RestController
@@ -19,7 +26,25 @@ public class SpringbootLayeredApiTemplateApplication {
 	
 	@PostMapping("/register")
 	public String register(@RequestBody User user) {
-		repository.save
+		repository.save(user);
+		return "Hello, "+ user.getUsername() + ", your registration was completed successfully!";
+	}
+	
+	@GetMapping("/getAllUsers")
+	public List<User> findAllUsers() {
+		return (List<User>) repository.findAll();
+	}
+	
+	@GetMapping("/getUser/{email}")
+	public List<User> findUser(@PathVariable String email) {
+		return repository.findAllByEmail(email);
+		
+	}
+	
+	@DeleteMapping("/cancel/{id}")
+	public List<User> findUser(@PathVariable Integer id) {
+		repository.deleteById(id);
+		return repository.findAll();
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/ntolb/layeredapi/data/UserRepository.java
+++ src/main/java/com/ntolb/layeredapi/data/UserRepository.java
@@ -1,10 +1,177 @@
 package com.ntolb.layeredapi.data;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public class UserRepository implements CrudRepository {
+import com.ntolb.layeredapi.model.User;
+
+public class UserRepository implements JpaRepository {
+
+	/*
+	 *                 T   O   D   O   ! ! !
+	 * 
+	 *           IMPLEMENT NEEDED! -->  UNNEEDED GET DELETED!
+	 * 
+	 * 
+	 */
+	
+	
+	
+	public List<User> findAllByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object save(Object entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional findById(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(Object id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Object entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll(Iterable entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional findOne(Example example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page findAll(Example example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count(Example example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean exists(Example example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List findAll(Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List findAllById(Iterable ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List saveAll(Iterable entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object saveAndFlush(Object entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteInBatch(Iterable entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getOne(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List findAll(Example example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List findAll(Example example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 
